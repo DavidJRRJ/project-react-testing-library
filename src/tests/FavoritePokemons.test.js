ca39d3b739ee4654b8b7fcf48c73a14a2b894e79
David <david.lourenco94@gmail.com>
2022-07-18 17:12:42 -0300

    Cobertura de testes do componente FavoritePokemons 50%

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,12 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../helpers/renderWithRouter';
+import { FavoritePokemons } from '../pages';
+
+describe('Teste do componente FavoritePokemons', () => {
+  test('Verifica a mensagem de quando não há nenhum pokemon adicionado', () => {
+    renderWithRouter(<FavoritePokemons />);
+    screen.getByText(/no favorite pokemon found/i);
+  });
+});
