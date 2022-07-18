e8584c8d7ce8a6105db487859f0b7eb56f1f15bf
David <david.lourenco94@gmail.com>
2022-07-18 17:20:47 -0300

    Cobertura de testes do componente NotFound Completa

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,25 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../helpers/renderWithRouter';
+import { NotFound } from '../pages';
+
+describe('Teste do componente NotFound', () => {
+  test('Testa se a página contém um h2 com uma msg de erro', () => {
+    renderWithRouter(<NotFound />);
+    screen.getByRole('heading', {
+      name: /page requested not found/i,
+    });
+  });
+
+  test('Testa se a página mostra uma imagem com link específico', () => {
+    renderWithRouter(<NotFound />);
+    const img = screen.getByRole('img', {
+      name: /pikachu crying because the page requested was not found/i,
+    });
+    expect(img).toHaveAttribute(
+      'src',
+      'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif',
+    );
+  });
+});
